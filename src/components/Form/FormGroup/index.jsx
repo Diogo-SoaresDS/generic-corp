349071b1843d9c1737b878b82528e8decf488273
Diogo-SoaresDS <diogo.soares10.ds.ds@gmail.com>
2024-09-15 17:26:12 -0300

    Customizing the Input Components

--- src/components/Form/FormGroup/index.jsx
+++ src/components/Form/FormGroup/index.jsx
@@ -1,8 +1,39 @@
+import { useState } from 'react';
+import EyeIcon from '../../../assets/visibility_24.svg';
+import EyeOffIcon from '../../../assets/visibility_off_24.svg';
+
 export default function FormGroup({ label, type, name, placeholder }) {
+    const [isPasswordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!isPasswordVisible);
+    };
+
     return (
-        <div>
-            <label htmlFor={name}>{label}</label>
-            <input type={type} id={name} name={name} placeholder={placeholder} />
+        <div className="relative flex flex-col">
+            <label className="text-xs ml-4 font-medium text-gray-500 mb-1" htmlFor={name}>
+                {label}
+            </label>
+            <input
+                className="px-4 py-3 rounded-md bg-gray-100 placeholder-gray-500 focus:outline-none w-full"
+                type={type === 'password' ? (isPasswordVisible ? 'text' : 'password') : type}
+                id={name}
+                name={name}
+                placeholder={placeholder}
+            />
+            {type === 'password' && (
+                <button
+                    type="button"
+                    className="absolute inset-y-0 right-0 pr-5 pt-5 flex items-center cursor-pointer"
+                    onClick={togglePasswordVisibility}
+                >
+                    <img
+                        src={isPasswordVisible ? EyeOffIcon : EyeIcon}
+                        alt={isPasswordVisible ? 'Hide password' : 'Show password'}
+                        className="h-6 w-6 text-gray-500"
+                    />
+                </button>
+            )}
         </div>
-    )
+    );
 }
